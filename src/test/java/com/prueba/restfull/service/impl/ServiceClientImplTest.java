92ccbeb24e73a1cdf458d814fe535ec8a13f9dc6
Javier Garcia <javiergarciadevcol@gmail.com>
2024-07-03 13:32:39 -0500

    Initial commit

--- src/test/java/com/prueba/restfull/service/impl/ServiceClientImplTest.java
+++ src/test/java/com/prueba/restfull/service/impl/ServiceClientImplTest.java
@@ -32,23 +32,22 @@ public class ServiceClientImplTest {
 
     @Test
     void testFindClienteByDocument() {
-        // Datos de prueba
         Long documento = 123456789L;
         Client client = new Client(documento, "DNI", "Juan Pérez", null);
         ClientModel clientModel = new ClientModel(documento, "DNI", "Juan Pérez", null);
 
-        // Mock del repositorio
+
         when(repositoryClient.findByDocumento(documento)).thenReturn(Mono.just(client));
 
-        // Mock del mapper
+
         when(mapperClient.toClientModel(client)).thenReturn(clientModel);
 
-        // Ejecutar el método del servicio y verificar el resultado
+
         Mono<ClientModel> result = serviceClient.findClienteByDocument(documento);
         StepVerifier.create(result)
                 .expectNextMatches(model -> model.getDocumento().equals(documento))
                 .verifyComplete();
     }
 
-    // Otras pruebas para saveClient y otros métodos del servicio pueden ser agregadas aquí
+
 }
